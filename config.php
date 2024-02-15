a0fa3140d10d86a925aa2922d3ec991cb41ffdd6
Lucas Matheus de Campos Silva <63106126+LucasM1710@users.noreply.github.com>
2024-02-15 09:07:21 -0300

    Update config.php

--- config.php
+++ config.php
@@ -22,8 +22,8 @@
 		};
 		spl_autoload_register($autoload);
 
-	define('INCLUDE_PATH','http://portal.eranalitica.com.br/');
-	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+	define('INCLUDE_PATH','http://portal.eranalitica.com.br');
+	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'/painel/');
 
 	define('BASE_DIR_PAINEL',__DIR__.'/painel');
 
