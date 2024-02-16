f8d191e76b873a170226fadd2a7f55635fa5411d
developerro <emanuelsantana808@gmail.com>
2024-02-16 16:19:06 -0300

    add composer

--- painel/index.php
+++ painel/index.php
@@ -1,5 +1,7 @@
 <?php
 ob_start();
+Sentry\init(['dsn' => 'http://e6308909af1d4585b9a386ec50b0792c@ermonitor.eranalitica.com.br/1' ]);
+
 	include('../config.php');
  
 	if(Painel::logado() == false){
