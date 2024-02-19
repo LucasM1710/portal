f62b706454161af37af99ace139ef2b2304996c8
Lucas Matheus de Campos Silva <63106126+LucasM1710@users.noreply.github.com>
2024-02-19 10:45:12 -0300

    trocando url no config

--- config.php
+++ config.php
@@ -19,7 +19,7 @@
 		};
 		spl_autoload_register($autoload);
 
-	define('INCLUDE_PATH','http://217.196.62.185:8012/');
+	define('INCLUDE_PATH','http://portal.eranalitica.com.br/');
 	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
 
 	define('BASE_DIR_PAINEL',__DIR__.'/painel');
